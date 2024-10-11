cf9ac379547989a6e9ce6a512b4f49b3214ec9f0
Mohammed Shamil <kmshamil123@gmail.com>
2024-10-11 22:11:20 +0530

    updated about page

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -5,6 +5,6 @@ export const aboutMeData = [
 	},
 	{
 		id: 2,
-		bio: 'I am a dedicated and passionate computer science student with a keen interest in web development. My focus lies in both front-end and back-end technologies, where I enjoy crafting engaging user interfaces as much as optimizing server-side functionalities. ',
+		bio: 'I am a dedicated and passionate computer science graduate with a keen interest in web development. My focus lies in both front-end and back-end technologies, where I enjoy crafting engaging user interfaces as much as optimizing server-side functionalities. I am currently on the lookout for opportunities as React Developer. I currently reside in United Arab Emirates.',
 	},
 ];
