be90c38119fe94080db42c54f609bfd30da5db23
Mohammed Shamil <kmshamil123@gmail.com>
2024-10-11 22:00:46 +0530

    Initial

---
+++ src/components/shared/AppBanner.jsx
@@ -0,0 +1,115 @@
+import useThemeSwitcher from "../../hooks/useThemeSwitcher";
+import { FiArrowDownCircle } from "react-icons/fi";
+import { motion } from "framer-motion";
+import { TypeAnimation } from "react-type-animation";
+import css3 from "../../images/tools/css3.svg";
+import expressjs from "../../images/tools/expressjs.svg";
+import figma from "../../images/tools/figma.svg";
+import javascript from "../../images/tools/javascript.svg";
+import mongodb from "../../images/tools/mongodb.svg";
+import nodejs from "../../images/tools/nodejs.svg";
+import react from "../../images/tools/react.svg";
+import tailwindcss from "../../images/tools/tailwindcss.svg";
+
+const AppBanner = () => {
+  const [activeTheme] = useThemeSwitcher();
+
+  return (
+    <div className="">
+      <motion.section
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }}
+        className="flex flex-col sm:justify-between items-center sm:flex-row mt-12 md:mt-7"
+      >
+        <div className="">
+          <div className="image  flex justify-center">
+            {/* <img className="object-cover" src={Profile} height={300} width={300} alt="image description"/> */}
+            <img
+              src="https://placehold.co/300x300"
+              className="flex justify-center mb-12"
+              alt=""
+            />
+          </div>
+          <div className="w-full">
+            <motion.h1
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                ease: "easeInOut",
+                duration: 0.9,
+                delay: 0.1,
+              }}
+              className="name flex justify-center font-general-semibold text-4xl lg:text-4xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase"
+            >
+              Hi, I'm Mohammed Shamil
+            </motion.h1>
+
+            <div className="flex justify-center">
+              <TypeAnimation
+                sequence={[
+                  "A full stack developer",
+                  1500,
+                  "A Front end developer",
+                  1500,
+                  "A Backend developer",
+                  1500,
+                  "A React developer",
+                  1500,
+                ]}
+                wrapper="span"
+                speed={30}
+                style={{ fontSize: "2em", display: "inline-block" }}
+                repeat={Infinity}
+                className="font-general-medium mt-4 text-lg md:text-xl lg:text-1xl xl:text-3xl text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
+              />
+            </div>
+
+            <div className="flex justify-center">
+              <a
+                download="Mohammed_Shamil.pdf"
+                href="/files/Mohammed_Shamil.pdf"
+                className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
+                aria-label="Download Resume"
+              >
+                <FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
+                <span className="text-sm sm:text-lg font-general-medium duration-100">
+                  Download CV
+                </span>
+              </a>
+            </div>
+          </div>
+          <div className="text-center mt-10">
+            <div>
+              <h5 className="font-semibold  text-ternary-dark dark:text-primary-light">Tools I use</h5>
+            </div>
+            <div className="border rounded-lg bg-gray-600 mt-5">
+              <ul className="flex justify-center p-4 gap-10 px-8">
+                <li>
+                  <img src={css3} alt="" />
+                </li>
+                <li>
+                  <img src={javascript} alt="" />
+                </li>
+                <li>
+                  <img src={react} alt="" />
+                </li>
+                <li>
+                  <img src={nodejs} alt="" />
+                </li>
+                <li>
+                  <img src={mongodb} alt="" />
+                </li>
+                <li>
+                  <img src={tailwindcss} alt="" />
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </motion.section>
+    </div>
+  );
+};
+
+export default AppBanner;
---
+++ src/data/projects.js
@@ -0,0 +1,44 @@
+// Import images
+import Landing from '../images/landing4.png'
+import Fashion from '../images/fashion1.png';
+import Hotel from '../images/bookmyhotel1.png';
+import Blog from '../images/blog1.png';
+import Web3 from '../images/web3.png';
+
+export const projectsData = [
+	{
+		id: 1,
+		title: 'Virtual World',
+		category: 'React',
+		img: Landing,
+		ProjectHeader: {
+			title: 'Project Management UI - From Context',
+			publishDate: 'Jul 26, 2021',
+			tags: 'UI / Frontend',
+		},
+	},
+	{
+		id: 2,
+		title: 'Fashion Store',
+		category: 'React',
+		img: Fashion,
+	},
+	{
+		id: 3,
+		title: 'Blog App',
+		category: 'Full stack',
+		img: Blog,
+	},
+	{
+		id: 4,
+		title: 'Book my Hotel',
+		category: 'Full stack',
+		img: Hotel,
+	},
+	{
+		id: 5,
+		title: 'Web3 Crowdfunding',
+		category: 'React',
+		img: Web3,
+	},
+];
