a13bf5d4660f4c8aab00ed7188066fda611f7375
Mohammed Shamil <kmshamil123@gmail.com>
2024-10-14 20:30:02 +0530

    Updated my skills logos

--- src/components/about/AboutMeBio.jsx
+++ src/components/about/AboutMeBio.jsx
@@ -1,12 +1,11 @@
 import profileImage from "../../images/profile.jpeg";
 import { useContext } from "react";
 import AboutMeContext from "../../context/AboutMeContext";
-import css3 from "../../images/tools/css3.svg";
-import expressjs from "../../images/tools/expressjs.svg";
+import css3big from "../../images/tools/css3-big.svg";
+import expressjsbig from "../../images/tools/express-big.svg";
 import figmabig from "../../images/tools/figma-big.svg";
-import javascript from "../../images/tools/javascript.svg";
-import mongodb from "../../images/tools/mongodb.svg";
-import nodejs from "../../images/tools/nodejs.svg";
+import mongodbbig from "../../images/tools/mongodbbig.svg";
+import nodejsbig from "../../images/tools/nodejs-big.svg";
 import react from "../../images/tools/react.svg";
 import tailwindcss from "../../images/tools/tailwindcss.svg";
 import typescript from '../../images/tools/typescript.svg';
@@ -37,14 +36,15 @@ const AboutMeBio = () => {
           <h1>My Skills</h1>
           <div className=""> 
             <div className="flex justify-center gap-10">
+				<div className="skills"><img src={javascriptbig} alt="" /></div>
 				<div className="skills"><img src={typescriptbig} alt="" /></div>
-				<div className="skills"><img src={javascriptbig} alt="" /></div>
-				<div className="skills"><img src={javascriptbig} alt="" /></div>
-				<div className="skills"><img src={javascriptbig} alt="" /></div>
+				<div className="skills"><img src={nodejsbig} alt="" /></div>
+				<div className="skills"><img src={expressjsbig} alt="" /></div>
             </div>
 			<div className="flex justify-center gap-10">
-				<div className="skills"><img src={javascriptbig} alt="" /></div>
-				<div className="skills"><img src={javascriptbig} alt="" /></div>
+				<div className="skills"><img src={mongodbbig} alt="" /></div>
+				<div className="skills"><img src={figmabig} alt="" /></div>
+				<div className="skills"><img src={css3big} alt="" /></div>
 				<div className="skills"><img src={javascriptbig} alt="" /></div>
 			</div>
           </div>
