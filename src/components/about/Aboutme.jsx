0ba7c9884c5ec8405b680a8e7aba661317a29e22
Mohammed Shamil <kmshamil123@gmail.com>
2024-10-20 13:15:01 +0530

    Updated skills redirect

--- src/components/about/Aboutme.jsx
+++ src/components/about/Aboutme.jsx
@@ -23,8 +23,9 @@ const Aboutme = () => {
       <div className="flex flex-col justify-center font-general-regular font-semibold mt-14 text-center">
         <h1 className="text-4xl">Hello World! 👋</h1>
         <p className="mt-10 container">
-          I'm Mohammed, a computer science graduate with a passion for transforming
-          lines of code into seamless <span className="">user experiences</span>. When I'm not busy
+          I'm Mohammed, a computer science graduate with a passion for
+          transforming lines of code into seamless{" "}
+          <span className="">user experiences</span>. When I'm not busy
           debugging, you can find me dreaming up the next big thing in web
           development. I specialize in making the web a more beautiful place
           with my React wizardry. From sleek front-end designs to smooth user
@@ -36,25 +37,61 @@ const Aboutme = () => {
         <div className="flex flex-col justify-center text-center">
           <h1>My Skills</h1>
           <div className="flex justify-center">
-          <div className="mt-10 skills">
-            <img src={htmlbig} alt="HTML" className="p-5 " />
-            <img src={css3big} alt="CSS3" className="p-5" />
-            <img src={javascriptbig} alt="JavaScript" className="p-5" />
-            <img src={typescriptbig} alt="TypeScript" className="p-5" />
-            <img src={reactbig} alt="React" className="p-5" />
-            <img src={vitebig} alt="Vite" className="p-5" />
-            <img src={nodejsbig} alt="Node.js" className="p-5" />
-            <img src={expressjsbig} alt="Express.js" className="p-5" />
-            <img src={tailwindcssbig} alt="Tailwind CSS" className="p-5" />
-            <img src={figmabig} alt="Figma" className="p-5" />
-            <img src={mongodbbig} alt="MongoDB" className="p-5" />
-            <img src={chakrabig} alt="Chakra UI" className="p-5" />
-            <img src={bootstrapbig} alt="Bootstrap" className="p-5" />
-            <img src={java} alt="Java" className="p-5" />
-            <img src={cbig} alt="C" className="p-5" />
-            <img src={postmanbig} alt="Postman" className="p-5" />
-            <img src={npm} alt="NPM" className="p-5" />
-          </div>
+            <div className="mt-10 skills">
+              <a href="https://en.wikipedia.org/wiki/HTML5#:~:text=HTML5%20(Hypertext%20Markup%20Language%205,as%20the%20HTML%20Living%20Standard.">
+                <img src={htmlbig} alt="HTML" className="p-5 " />
+              </a>
+              <a href="https://en.wikipedia.org/wiki/CSS">
+                <img src={css3big} alt="CSS3" className="p-5" />
+              </a>
+              <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+                <img src={javascriptbig} alt="JavaScript" className="p-5" />
+              </a>
+
+              <a href="https://developer.mozilla.org/en-US/docs/Glossary/TypeScript">
+                <img src={typescriptbig} alt="TypeScript" className="p-5" />
+              </a>
+
+              <a href="https://www.react.dev/">
+                <img src={reactbig} alt="React" className="p-5" />
+              </a>
+              <a href="https://www.vite.dev/">
+                <img src={vitebig} alt="Vite" className="p-5" />
+              </a>
+              <a href="https://www.nodejs.org/en">
+                <img src={nodejsbig} alt="Node.js" className="p-5" />
+              </a>
+              <a href="https://expressjs.com/">
+                <img src={expressjsbig} alt="Express.js" className="p-5" />
+              </a>
+              <a href="https://tailwindcss.com/">
+                <img src={tailwindcssbig} alt="Tailwind CSS" className="p-5" />
+              </a>
+              <a href="https://www.figma.com">
+                <img src={figmabig} alt="Figma" className="p-5" />
+              </a>
+              <a href="https://www.mongodb.com">
+                <img src={mongodbbig} alt="MongoDB" className="p-5" />
+              </a>
+              <a href="https://www.chakra-ui.com">
+                <img src={chakrabig} alt="Chakra UI" className="p-5" />
+              </a>
+              <a href="https://getbootstrap.com/">
+                <img src={bootstrapbig} alt="Bootstrap" className="p-5" />
+              </a>
+              <a href="https://en.wikipedia.org/wiki/Java_(programming_language)">
+                <img src={java} alt="Java" className="p-5" />
+              </a>
+              <a href="https://en.wikipedia.org/wiki/C_(programming_language)">
+                <img src={cbig} alt="C" className="p-5" />
+              </a>
+              <a href="https://www.postman.com">
+                <img src={postmanbig} alt="Postman" className="p-5" />
+              </a>
+              <a href="https://www.npmjs.com">
+                <img src={npm} alt="NPM" className="p-5" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
