486832a42fd02077b07468d305b62c33a3ee34e3
Mohammed Shamil <kmshamil123@gmail.com>
2024-10-22 09:27:44 +0530

    Fixed search

--- src/data/projects.js
+++ src/data/projects.js
@@ -25,7 +25,7 @@ export const projectsData = [
 		href: "https://furniture-s7xdd.netlify.app/"
 	},
 	{
-		id: 1,
+		id: 2,
 		title: 'News Portal',
 		category: 'React',
 		img: Newsportal,
@@ -37,7 +37,7 @@ export const projectsData = [
 		href: "https://newsportal-s7xdd.netlify.app/"
 	},
 	{
-		id: 1,
+		id: 3,
 		title: 'Hotelier',
 		category: 'React',
 		img: Hotel1,
@@ -49,7 +49,7 @@ export const projectsData = [
 		href: "https://hotelanding-s7xdd.netlify.app/"
 	},
 	{
-		id: 1,
+		id: 4,
 		title: 'Virtual World',
 		category: 'React',
 		img: Landing,
@@ -61,49 +61,49 @@ export const projectsData = [
 		href: "https://virtualworld-s7xdd.netlify.app/"
 	},
 	{
-		id: 2,
+		id: 5,
 		title: 'Fashion Store',
 		category: 'React',
 		img: Fashion,
 		href: "https://fashionstore-s7xdd.netlify.app/",
 	},
 	{
-		id: 2,
+		id: 6,
 		title: 'Manage',
 		category: 'React',
 		img: Manage,
 		href: "https://landing1-s7xdd.netlify.app/",
 	},
 	{
-		id: 3,
+		id: 7,
 		title: 'Blog App',
 		category: 'Full stack',
 		img: Blog,
 		href: "https://blog-s7xdd.onrender.com/",
 	},
 	{
-		id: 4,
+		id: 8,
 		title: 'News App',
 		category: 'Full stack',
 		img: Reactnews,
 		href: "https://react-news-s7xdd.netlify.app/",
 	},
 	{
-		id: 4,
+		id: 9,
 		title: 'Book my Hotel',
 		category: 'Full stack',
 		img: Hotel,
 		href: "https://github.com/s7xdd/BookMyHotel",
 	},
 	{
-		id: 5,
+		id: 10,
 		title: 'Web3 Crowdfunding',
 		category: 'React',
 		img: Web3,
 		href: "https://dapp-s7xdd.netlify.app/",
 	},
 	{
-		id: 5,
+		id: 11,
 		title: 'Age Calculator',
 		category: 'Mobile Application',
 		img: Android,
